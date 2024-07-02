bd72f85def141d38d8505899511c7a1c3e7db661
abrahamuchos <j.abraham29@gmail.com>
2024-07-02 16:06:14 -0400

    Final home structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Col } from "antd";
+import Nav from "./componets/Nav/index.jsx";
 import Searcher from "./componets/Searcher.jsx";
 import PokemonList from "./componets/PokemonList";
 
@@ -9,12 +10,23 @@ function App() {
 
   return (
     <>
+      {/*Navigation*/}
+      <Col>
+        <Nav/>
+      </Col>
+      {/*End Navigation*/}
+
+      {/*Search bar*/}
       <Col span={8} offset={8}>
         <Searcher/>
       </Col>
+      {/*End Search bar*/}
+
+      {/*List*/}
       <Col span={24}>
         <PokemonList/>
       </Col>
+      {/*End List*/}
     </>
   )
 }
---
+++ src/componets/Nav/index.jsx
@@ -0,0 +1,13 @@
+import logo from '../../assets/logo.svg'
+
+import './styles.css';
+
+export default function Nav() {
+
+
+  return (
+    <nav>
+      <img src={logo} alt="pokedux-logo"/>
+    </nav>
+  );
+}
